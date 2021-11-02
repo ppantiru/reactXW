23e982980d72a9d4b586773c39139b592bbe23e4
Paul Pantiru <paul.pantiru@xwiki.com>
2021-11-02 09:26:46 +0200

    Initial react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import {XPage} from './components/XPage';
+import { BrowserRouter as Router, Route} from 'react-router-dom'
 
-function App() {
+export default function App() {
+  
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div style={{width: '100%'}}>
+          <Router>
+            <Route exact path="/bin/view/:space/:page">
+              <XPage />
+            </Route>
+            <Route exact path="/bin/view/:space/">
+              <XPage />
+            </Route>
+          </Router>
+        </div>
       </header>
     </div>
   );
 }
-
-export default App;
---
+++ src/components/XPage.js
@@ -0,0 +1,24 @@
+import useFetch from './useFetch';
+import parse, { domToReact } from 'html-react-parser';
+import { Link, useParams } from 'react-router-dom'
+
+const XPage = () => {
+  const {space, page} = useParams()
+  const [data] = useFetch(`/bin/view/${space}/${page ? page : 'WebHome'}`);
+  const pageContent = data;
+  return parseWithLinks(`${pageContent}`);
+};
+
+  function parseWithLinks(text) {
+    const options = {
+      replace: ({ name, attribs, children }) => {
+        if (name === 'a' && attribs.href) {
+          return <Link to={attribs.href}>{domToReact(children)}</Link>;
+        }
+      }
+    };
+        
+    return parse(text, options);
+  }
+
+export {XPage}
